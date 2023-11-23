0300a29e5ee12a67ae35cee78e9542e25cf12291
TetRey <reyhaneldwin31@gmail.com>
2023-11-23 11:02:38 +0700

    trying gitignore

--- public/js/product.js
+++ public/js/product.js
@@ -12,3 +12,17 @@ function updateQuantity(change) {
   // Update the input value
   input.value = newValue;
 }
+function toggleHeartIcon() {
+  var heartIcon = document.getElementById('heartIcon');
+
+  // Toggle between 'fa-regular' and 'fa-solid' classes
+  if (heartIcon.classList.contains('fa-regular')) {
+    heartIcon.classList.remove('fa-regular');
+    heartIcon.classList.add('fa-solid');
+    heartIcon.style.color = '#6c4933'; // Change color to red for solid heart
+  } else {
+    heartIcon.classList.remove('fa-solid');
+    heartIcon.classList.add('fa-regular');
+    heartIcon.style.color = '#6c4933'; // Change color back to the original color for regular heart
+  }
+}
